ded10cc320ba43315a5a3f44b81d9d7e59978129
Florian Eickhorst <eickhorst@fleimedia.de>
2019-10-20 21:56:44 +0200

    svg upload: function replaceable, filter priority lowered

--- enable-svg-upload.php
+++ enable-svg-upload.php
@@ -1,7 +1,10 @@
 <?php
 
-function flei_mime_types($mimes) {
-	$mimes['svg'] = 'image/svg+xml';
-	return $mimes;
+if (!function_exists('flei_mime_types')) {
+    function flei_mime_types($mimes)
+    {
+        $mimes['svg'] = 'image/svg+xml';
+        return $mimes;
+    }
 }
-add_filter('upload_mimes', 'flei_mime_types');
+add_filter('upload_mimes', 'flei_mime_types',99,1);
