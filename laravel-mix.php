5c61e64a62602194a2cefa81e2336d8955f76bf0
Florian Eickhorst <eickhorst@fleimedia.de>
2019-10-20 21:57:47 +0200

    laravel mix handling: manifest file handling improved

--- laravel-mix.php
+++ laravel-mix.php
@@ -17,42 +17,48 @@ if (!function_exists('mix')) {
      */
     function mix($path, $manifestDirectory = '', $format = 'url')
     {
-        static $manifest;
-        $publicFolder = '/dist';
-        $rootPath = get_stylesheet_directory();
-        $publicPath = $rootPath . $publicFolder;
-        if ($manifestDirectory && substr($manifestDirectory, 0, 1) != '/') {
-            $manifestDirectory = "/{$manifestDirectory}";
-        }
-
-        if (!$manifest) {
-            if (!file_exists($manifestPath = ($publicPath . $manifestDirectory . '/mix-manifest.json'))) {
-                throw new Exception('The Mix manifest does not exist.');
-            }
-            $manifest = json_decode(file_get_contents($manifestPath), true);
-        }
-
         if (substr($path, 0, 1) != '/') {
             $path = "/{$path}";
         }
 
-        if (!array_key_exists($path, $manifest)) {
-            throw new Exception(
-                "Unable to locate Mix file: {$path}. Please check your " .
-                'webpack.mix.js output paths and try again.'
-            );
+        static $manifest;
+        $publicFolder = '/dist';
+        $rootPath = get_stylesheet_directory();
+        $publicPath = $rootPath . $publicFolder;
+
+        $path_suffix = $path;
+
+        if ($manifestDirectory && substr($manifestDirectory, 0, 1) != '/') {
+            $manifestDirectory = "/{$manifestDirectory}";
+        }
+
+        if (!$manifest || (is_numeric($manifest) && $manifest < 0)) {
+            if (!file_exists($manifestPath = ($publicPath . $manifestDirectory . '/mix-manifest.json'))) {
+//                throw new Exception('The Mix manifest does not exist.');
+                $manifest = -1; // no manifest
+            } else {
+                $manifest = json_decode(file_get_contents($manifestPath), true);
+                if ($manifest && !array_key_exists($path, $manifest)) {
+                    throw new Exception(
+                        "Unable to locate Mix file: {$path}. Please check your " .
+                        'webpack.mix.js output paths and try again.'
+                    );
+                } else {
+                    $path_suffix = $manifest[$path];
+                }
+            }
         }
 
         $path_prefix = $publicPath;
 
         if ($format == 'url') {
-            $path_prefix = get_stylesheet_directory_uri().$publicFolder;
+            $path_prefix = get_stylesheet_directory_uri() . $publicFolder;
         } elseif ($format == 'absolute') {
             $path_prefix = get_stylesheet_directory();
         } elseif ($path_prefix === '' || $path_prefix === false) {
             $path_prefix = '';
         }
 
-        return $path_prefix . $manifest[$path];
+        return $path_prefix . $path_suffix;
     }
 }
