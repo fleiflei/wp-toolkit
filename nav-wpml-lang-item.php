52b08ffd63fc2727c148498ad40946dc32745017
Florian Eickhorst <eickhorst@fleimedia.de>
2021-10-07 14:34:51 +0200

    updated wpml nav lang items

--- nav-wpml-lang-item.php
+++ nav-wpml-lang-item.php
@@ -1,31 +1,31 @@
 <?php
 
 if (function_exists('icl_get_languages')):
-  add_filter('wp_nav_menu_items', 'add_lang_menu_item', 10, 2);
+    add_filter('wp_nav_menu_items', 'add_lang_menu_item', 10, 2);
 
-  function add_lang_menu_item($items, $args)
-  {
-    $languages = icl_get_languages('skip_missing=1');
+    function add_lang_menu_item($items, $args)
+    {
+        $languages = icl_get_languages('skip_missing=1');
 
-    if ($args->theme_location == 'primary_navigation' && 1 < count($languages)) {
-      $items .= '<li class="language">';
+        if ($args->theme_location == 'primary_navigation' && 1 < count($languages)) {
+            $items .= '<li class="language">';
 
-      $counter = 0;
-      foreach ($languages as $l) {
-        $counter++;
-        $items .= '<a href="'.$l['url'].'"';
+            $counter = 0;
+            foreach ($languages as $idx => $l) {
+                $counter++;
+                $items .= '<a href="' . $l['url'] . '"';
 
-        $items .= ($l['active'] ? ' class="active"' : '');
+                $items .= ($l['active'] ? ' class="active"' : '');
 
-        $items .= '>'.$l['code'].'</a>';
-        //            $items .= ($counter < count($languages)?' | ':'');
+                $items .= '>' . $l['code'] . '</a>';
 
-        //            if(!$l['active']) $langs[] = '<a href="'.$l['url'].'">'.$l['translated_name'].'</a>';
-        //            if (!$l['active']) $items .= '<li id="menu-item-lang-switch" class="menu-item menu-item-type-custom"><a href="' . $l['url'] . '">' . $l['native_name'] . '</a></li>';
-      }
-      $items .= '</li>';
+                if ($counter < (count($languages))) {
+                    $items .= ' | ';
+                }
+            }
+            $items .= '</li>';
+        }
+
+        return $items;
     }
-
-    return $items;
-  }
 endif;
