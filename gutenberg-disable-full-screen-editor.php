f6d5987095eabf096d11984355dc2b18b020f3d2
Florian Eickhorst <eickhorst@fleimedia.de>
2021-10-07 14:32:10 +0200

    gutenberg: disable full screen editor by default

---
+++ gutenberg-disable-full-screen-editor.php
@@ -0,0 +1,9 @@
+<?php
+
+if (is_admin()) {
+    add_action('enqueue_block_editor_assets', function () {
+        $script = "jQuery( window ).load(function() { const isFullscreenMode = wp.data.select( 'core/edit-post' ).isFeatureActive( 'fullscreenMode' ); if ( isFullscreenMode ) { wp.data.dispatch( 'core/edit-post' ).toggleFeature( 'fullscreenMode' ); } });";
+        wp_add_inline_script('wp-blocks', $script);
+    });
+
+}
